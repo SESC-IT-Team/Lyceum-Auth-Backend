893b8bdb691adb4f221d89f6333d575cab54a9f9
Sr_lucky_MaN <d.dunin1709@gmail.com>
2026-08-14 20:28:18 +0500

    Auth router destroyed

--- app/presentation/api/v1/__init__.py
+++ app/presentation/api/v1/__init__.py
@@ -1,11 +1,9 @@
 from fastapi import APIRouter
 
-from app.presentation.api.v1.auth import router as auth_router
 from app.presentation.api.v1.department import router as department_router
 from app.presentation.api.v1.user import router as user_router
 
 router = APIRouter()
-router.include_router(auth_router, tags=['Auth'])
 router.include_router(department_router)
 router.include_router(user_router)
 
--- app/presentation/api/v1/auth.py
+++
@@ -1,5 +0,0 @@
-from sesc_auth_sdk.routers.auth_router import create_auth_router
-
-from app.config import settings
-
-router = create_auth_router(settings.auth_router_settings)
